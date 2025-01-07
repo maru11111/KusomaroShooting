ccb7f17626ebd918949faa1696ccf7d6888b86fb
maru <toshidai.tarou11111@gmail.com>
2025-01-08 01:35:49 +0900

    Quad二つを扱うためのクラス

---
+++ KusomaroShooting/TowQuads.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+# include<Siv3D.hpp> // Siv3D v0.6.14
+
+class TwoQuads {
+public:
+	Array<Quad>quads;
+
+	TwoQuads(Quad quad1) {
+		quads << quad1;
+	}
+
+	TwoQuads(Quad quad1, Quad quad2) {
+		quads << quad1;
+		quads << quad2;
+	}
+
+	bool collision(RectF rect) {
+		bool isIntersects=false;
+		for (int i = 0; i < quads.size(); i++) {
+			if (quads[i].intersects(rect)) {
+				isIntersects = true;
+			}
+		}
+		if (isIntersects) return true;
+		else return false;
+	}
+};
