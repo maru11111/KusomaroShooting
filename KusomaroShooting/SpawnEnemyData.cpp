f6fcd2e7d83b977c794142c35660735753a03d52
maru <toshidai.tarou11111@gmail.com>
2025-01-07 23:36:39 +0900

    出現する敵を格納する構造体

---
+++ KusomaroShooting/SpawnEnemyData.cpp
@@ -0,0 +1,4 @@
+﻿#include "SpawnEnemyData.h"
+
+double SpawnEnemyData::spawnTimer = 0;
+double SpawnEnemyData::spawnStartTime = 0;
---
+++ KusomaroShooting/SpawnEnemyData.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "Common.h"
+struct SpawnEnemyData {
+public:
+	int time;
+	Vec2 pos;
+	EnemyType type;
+	static double spawnTimer;
+	static double spawnStartTime;
+
+	bool isSpawnable()const {
+		//出現時間が試運転開始時間より後 かつ 出現時間のとき
+		if (spawnStartTime <= time && time <= spawnTimer) return true;
+		else return false;
+	}
+};
