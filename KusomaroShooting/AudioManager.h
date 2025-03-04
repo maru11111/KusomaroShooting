4252abd2e0d97c90a5266a0625900c549dde45cc
maru <toshidai.tarou11111@gmail.com>
2025-03-04 23:34:33 +0900

    SEをループできるように、すべてのSEを消すメソッドを追加

--- KusomaroShooting/AudioManager.h
+++ KusomaroShooting/AudioManager.h
@@ -24,13 +24,21 @@ public:
 		if(AudioType==AudioType::BGM)AudioAsset::Register(name, path, Loop::Yes);
 		else AudioAsset::Register(name, path);
 		AudioAsset::Load(name);
-		AudioTypes[name] = AudioType;
+		audioTypes[name] = AudioType;
+	}
+	// + SEをループするかどうか
+	void setAudio(String name, String path, AudioType AudioType, Loop loop) {
+		if (AudioType == AudioType::BGM)AudioAsset::Register(name, path, Loop::Yes);
+		else AudioAsset::Register(name, path, loop);
+		AudioAsset::Load(name);
+		audioTypes[name] = AudioType;
 	}
 
 	//audioを再生
 	//BGMかSEかによって異なる音量で再生
-	void play(String name) {
-		switch (AudioTypes[name]) {
+	//(BGMはつねにplay)
+	void playOneShot(String name) {
+		switch (audioTypes[name]) {
 		case AudioType::SE: AudioAsset(name).setVolume(seVolume).playOneShot(); break;
 		case AudioType::BGM:
 			//bgmのループ開始位置
@@ -47,13 +55,41 @@ public:
 			break;
 		}
 	}
+	void play(String name) {
+		switch (audioTypes[name]) {
+		case AudioType::SE: AudioAsset(name).setVolume(seVolume).play(); break;
+		case AudioType::BGM:
+			//bgmのループ開始位置
+			uint64 startSample;
+			if (name == U"MidBoss") {
+				startSample = (uint64)(39.724 * 44100);
+			}
+			else {
+				throw Error{ U"ループ位置が指定されていない、またはSEが正しく再生されていません。" };
+			}
+			//AudioAsset(name).seekSamples((uint64)66.207 * 44100);
+			AudioAsset(name).setLoopPoint(startSample);
+			AudioAsset(name).setVolume(bgmVolume).play();
+			break;
+		}
+	}
+
+	void stopAllSE() {
+		// 構造化束縛を使う
+		for (auto&& [key, value] : audioTypes)
+		{
+			if (value == AudioType::SE) {
+				AudioAsset(key).stop();
+			}
+		}
+	}
 
 private:
 	AudioManager() {}
 	static AudioManager* instance;
 	//曲とその種類のハッシュテーブル
-	HashTable<String, AudioType> AudioTypes;
+	HashTable<String, AudioType> audioTypes;
 	//音量
 	double seVolume=1.0;
-	double bgmVolume=1.0;
+	double bgmVolume=2.0;
 };
