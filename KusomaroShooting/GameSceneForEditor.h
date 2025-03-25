f278f903ba86ac4eda5406235bebfa1fd5881b76
maru <toshidai.tarou11111@gmail.com>
2025-03-25 22:12:56 +0900

    敵追加

--- KusomaroShooting/GameSceneForEditor.h
+++ KusomaroShooting/GameSceneForEditor.h
@@ -6,7 +6,10 @@ class GameSceneForEditor :public GameScene {
 public:
 	GameSceneForEditor(const InitData& init)
 		: GameScene(init)
-	{}
+	{
+		spawnEnemyData.clear();
+		currentStage = Stage::Editor;
+	}
 
 	void startPlayMode()const {
 		loadJson(U"stage/editorStage.json");
@@ -25,11 +28,15 @@ public:
 				switch (spawnEnemyData[i].type) {
 					case EnemyType::Empty: /*何もしない*/; break;
 					case EnemyType::Bag: objects.enemies << std::make_unique<GarbageBagNormal>(objects, spawnEnemyData[i].pos); break;
+					case EnemyType::AccelBag: objects.enemies << std::make_unique<GarbageBagAccel>(objects, spawnEnemyData[i].pos); break;
+					case EnemyType::SineBag: objects.enemies << std::make_unique<GarbageBagSine>(objects, spawnEnemyData[i].pos); break;
 					case EnemyType::FastBag: objects.enemies << std::make_unique<GarbageBagFast>(objects, spawnEnemyData[i].pos); break;
 					case EnemyType::BagWithCan: objects.enemies << std::make_unique<GarbageBagWithCan>(objects, spawnEnemyData[i].pos); break;
-					case EnemyType::Can: objects.enemies << std::make_unique<Can>(objects, spawnEnemyData[i].pos, objects.player->getPos()); break;
+					case EnemyType::BagWithCanStay: objects.enemies << std::make_unique<GarbageBagWithCanStay>(objects, spawnEnemyData[i].pos); break;
+					case EnemyType::Can: objects.enemies << std::make_unique<Can>(objects, spawnEnemyData[i].pos, Vec2{-1,0}); break;
 					case EnemyType::Fish: objects.enemies << std::make_unique<Fish>(objects, spawnEnemyData[i].pos); break;
 					case EnemyType::Umbrella: objects.enemies << std::make_unique<Umbrella>(objects, spawnEnemyData[i].pos); break;
+					case EnemyType::HealUmbrella: objects.enemies << std::make_unique<HealUmbrella>(objects, spawnEnemyData[i].pos); break;
 				}
 			}
 		}
