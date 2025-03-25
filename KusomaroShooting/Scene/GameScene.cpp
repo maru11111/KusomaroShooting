7a132238d0ceae94dc9e62ef3c8c13f8666e0b50
maru <toshidai.tarou11111@gmail.com>
2025-03-25 21:58:35 +0900

    敵を追加

--- KusomaroShooting/Scene/GameScene.cpp
+++ KusomaroShooting/Scene/GameScene.cpp
@@ -1353,11 +1353,15 @@ void GameScene::spawnEnemy() {
 			switch (spawnEnemyData[i].type) {
 			case EnemyType::Empty: /*何もしない*/; break;
 			case EnemyType::Bag: objects.enemies << std::make_unique<GarbageBagNormal>(objects, spawnEnemyData[i].pos); break;
+			case EnemyType::SineBag: objects.enemies << std::make_unique<GarbageBagSine>(objects, spawnEnemyData[i].pos); break;
+			case EnemyType::AccelBag: objects.enemies << std::make_unique<GarbageBagAccel>(objects, spawnEnemyData[i].pos); break;
 			case EnemyType::FastBag: objects.enemies << std::make_unique<GarbageBagFast>(objects, spawnEnemyData[i].pos); break;
 			case EnemyType::BagWithCan: objects.enemies << std::make_unique<GarbageBagWithCan>(objects, spawnEnemyData[i].pos); break;
+			case EnemyType::BagWithCanStay: objects.enemies << std::make_unique<GarbageBagWithCanStay>(objects, spawnEnemyData[i].pos); break;
 			case EnemyType::Can: objects.enemies << std::make_unique<Can>(objects, spawnEnemyData[i].pos, Vec2{-1,0}); break;
 			case EnemyType::Fish: objects.enemies << std::make_unique<Fish>(objects, spawnEnemyData[i].pos); break;
 			case EnemyType::Umbrella: objects.enemies << std::make_unique<Umbrella>(objects, spawnEnemyData[i].pos); break;
+			case EnemyType::HealUmbrella: objects.enemies << std::make_unique<HealUmbrella>(objects, spawnEnemyData[i].pos); break;
 			}
 		}
 	}
